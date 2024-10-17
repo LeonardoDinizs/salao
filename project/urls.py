322955ed9fe618c219aa8564172c876511880461
Leonardo Diniz <leonardo.diniz.s@gmail.com>
2024-10-17 14:31:00 -0300

    new file:   app/__pycache__/urls.cpython-311.pyc 	modified:   app/templates/index.html 	new file:   app/urls.py 	modified:   project/__pycache__/settings.cpython-311.pyc 	modified:   project/__pycache__/urls.cpython-311.pyc 	modified:   project/urls.py

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from app import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name='index.html'),
+    path('', include('app.urls')),
 ]
