7465d1863383eefcb22de25a924849ae4c3f91da
Leonardo Diniz <leonardo.diniz.s@gmail.com>
2024-10-25 17:50:05 -0300

    modified:   agendar/__pycache__/forms.cpython-311.pyc 	modified:   agendar/__pycache__/models.cpython-311.pyc 	modified:   agendar/__pycache__/urls.cpython-311.pyc 	modified:   agendar/__pycache__/views.cpython-311.pyc 	new file:   agendar/migrations/0001_initial.py 	new file:   agendar/migrations/__pycache__/0001_initial.cpython-311.pyc 	modified:   agendar/models.py 	modified:   agendar/static/agendar/css/agendamento_style.css 	new file:   agendar/static/agendar/imagens-agendar/form-agendar.jpg 	modified:   agendar/templates/agendar/agendamento.html 	modified:   project/__pycache__/settings.cpython-311.pyc 	modified:   project/settings.py

--- agendar/models.py
+++ agendar/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Agendamento(models.Model):
+    SERVIÇOS_CHOICES = [
+        ('corte_masculino', 'Corte Masculino'),
+        ('corte_feminino', 'Corte Feminino'),
+        ('corte_infantil', 'Corte Infantil'),
+        ('coloracao', 'Coloração'),
+        ('selagem', 'Selagem'),
+        ('hidratacao', 'Hidratação'),
+        ('progressiva', 'Progressiva Orgânica Sem Formol'),
+        ('outros', 'Outros'),
+    ]
+
+    nome = models.CharField(max_length=100)
+    celular = models.CharField(max_length=15)
+    email = models.EmailField(blank=True, null=True)  # Campo opcional
+    servicos = models.CharField(max_length=50, choices=SERVIÇOS_CHOICES)  # Pode ser ajustado para armazenar múltiplas escolhas
+    data_desejada = models.DateField()
+    horario_desejado = models.TimeField()
+    mensagem = models.TextField(blank=True, null=True)  # Campo opcional
+
+    def __str__(self):
+        return f"{self.nome} - {self.data_desejada} {self.horario_desejado}"
